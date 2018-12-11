db07d3e9a1c642abebca70d906044d300d148e40
sheshkumar prajapati <eluminous_se44@eluminoustechnologies.com>
2018-12-11 16:26:45 +0530

    added title in dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -7,10 +7,34 @@ use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
+	private $BaseModel;
+    private $ViewData;
+    private $JsonData;
+    private $ModuleTitle;
+    private $ModuleView;
+    private $ModulePath;
+
+    // use MultiModelTrait;
+
+    public function __construct(
+
+
+    )
+    {
+        $this->ViewData = [];
+        $this->JsonData = [];
+
+        $this->ModuleTitle = 'Dashboard';
+        $this->ModuleView = 'admin.dashboard';
+        $this->ModulePath = 'dashboard';
+    }
+
    	public function index()
    	{
-   		// dd('pass');
-   		
-   		return view('admin.dashboard');
+   		$this->ViewData['modulePath'] = $this->ModulePath;
+        $this->ViewData['moduleTitle'] = $this->ModuleTitle;
+        $this->ViewData['moduleAction'] = $this->ModuleTitle;
+
+        return view($this->ModuleView, $this->ViewData);
    	}
 }
---
+++ public/js/admin/prerequisite/index.js
@@ -0,0 +1,130 @@
+
+var $Path = $('meta[name="admin-path"]').attr('content');
+var $Module = '/prerequisite';
+
+$(document).ready(function() 
+{
+    var adminPath = $('meta[name="admin-path"]').attr('content');
+    var targetURL = adminPath+'/prerequisite/getPrerequisite'; 
+
+    $('#listingTable').DataTable( 
+    {
+        responsive: 'true',
+        serverSide: 'true',
+        processing: 'true',
+        ajax: targetURL,
+        
+        columns: [
+            { "data": "id",             "ordereble": "true"},
+            { "data": "title",          "ordereble": "true"},
+            { "data": "created_at",     "ordereble": "true"},
+            { "data": "status",         "ordereble": "true"},
+            { "data": "actions"}
+        ],
+
+        dom: 'Blfrtip',
+        buttons: [
+            'copyHtml5',
+            'excelHtml5',
+            'csvHtml5',
+            'pdfHtml5',
+            'print'
+        ],
+        lengthMenu: [[10, 25, 50, 100, 500], [10, 25, 50, 100, 500]],
+        aaSorting: [[0, 'DESC']]
+    } );
+});
+
+function rwDelete(element)
+{
+    var $this = $(element);
+    var id = $this.attr('data-rwid');
+
+    var $Action = '/'+id; 
+    var $URL    = $Path+$Module+$Action; 
+
+    if (id != '') 
+    {
+        swal({
+          title: "Are you sure !!",
+          text: "You want to delete ?",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Delete",
+          confirmButtonClass: "btn-danger",
+          closeOnConfirm: false,
+          showLoaderOnConfirm: true
+        }, 
+        function () 
+        {
+            $.ajax({
+                type:'DELETE',
+                url:$URL,
+                dataType:'json',
+                success: function(data)
+                {
+                    setTimeout(function () 
+                    {
+                        if (data.status == 'success') 
+                        {
+                            $('#listingTable').DataTable().ajax.reload();
+                            swal("Success", data.msg,'success');
+                        }
+                        else
+                        {
+                            swal("Error",data.msg,'error');
+                        }
+                    }, 1000);
+                }
+            })
+        });
+    } 
+}
+
+function rwChanceStatus(element)
+{
+    var $this   = $(element);
+    var id      = $this.attr('data-rwid');
+    var status  = $this.attr('data-rwst');
+     
+    var $Action     = '/changeStatus';
+    var $URL        = $Path+$Module+$Action; 
+
+    if (id != '') 
+    {
+        swal({
+          title: "Are you sure !!",
+          text: "You want to chaange status ?",
+          type: "warning",
+          showCancelButton: true,
+          confirmButtonText: "Change",
+          confirmButtonClass: "btn-warning",
+          closeOnConfirm: false,
+          showLoaderOnConfirm: true
+        }, 
+        function () 
+        {
+            $.ajax({
+                type:'POST',
+                data:{'id':id,'status':status},
+                url:$URL,
+                dataType:'json',
+                success: function(data)
+                {
+                    setTimeout(function () 
+                    {
+                        if (data.status == 'success') 
+                        {
+                            $('#listingTable').DataTable().ajax.reload();
+                            swal("Success", data.msg,'success');
+                        }
+                        else
+                        {
+                            swal("Error",data.msg,'error');
+                        }
+                    }, 1000);
+                }
+            })
+        });
+    } 
+}
