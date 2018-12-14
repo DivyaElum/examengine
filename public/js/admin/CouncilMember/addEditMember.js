c9657842227f186565bd8da941918d39f36ffb5d
ajay <eluminous_seajay@eluminoustechnologies.com>
2018-12-14 14:50:56 +0530

    updated the files

--- public/js/admin/CouncilMember/addEditMember.js
+++ public/js/admin/CouncilMember/addEditMember.js
@@ -42,29 +42,44 @@ function saveMember(element)
 	  	error: function (data)
 	  	{
 	  		$(element).closest('.box').LoadingOverlay("hide");
-	  		$('.form-group').removeClass('has-error');
-		   	$('.form-group').find('.help-block').html('');
-
-		   	$('.form-group').removeClass('has-error');
-			$('.form-group').find('.help-block').html('');
-
-	      	if( data.status === 422 ) 
-	      	{
-		      var errorBag = $.parseJSON(data.responseText);
-		      if (errorBag) 
-		       {
-		        $.each(errorBag.errors, function(row, fields)
-		        {
-		         $('.err_'+row).closest('.form-group').addClass('has-error');
-		         $('.err_'+row).html(fields);
-		          });
-		       }
+	  		if( data.status === 422 ) 
+  		 	{
+	  			var errorBag = $.parseJSON(data.responseText);
+		  		if (errorBag) 
+		    	{
+			    	$.each(errorBag.errors, function(row, fields)
+			    	{
+			    		toastr.error(fields);
+		      		});
+		    	}
 		    }
 		    else
 		    {
-		      toastr.error('Something went wrong, Please try again later.');
+	  			toastr.error('Something went wrong, Please try again later.');
 		    }
-	  		//toastr.error('Something went wrong, Please try again later.');
+	  		
+	  // 		$('.form-group').removeClass('has-error');
+		 //   	$('.form-group').find('.help-block').html('');
+
+		 //   	$('.form-group').removeClass('has-error');
+			// $('.form-group').find('.help-block').html('');
+
+	  //     	if( data.status === 422 ) 
+	  //     	{
+		 //      var errorBag = $.parseJSON(data.responseText);
+		 //      if (errorBag) 
+		 //       {
+		 //        $.each(errorBag.errors, function(row, fields)
+		 //        {
+		 //         $('.err_'+row).closest('.form-group').addClass('has-error');
+		 //         $('.err_'+row).html(fields);
+		 //          });
+		 //       }
+		 //    }
+		 //    else
+		 //    {
+		 //      toastr.error('Something went wrong, Please try again later.');
+		 //    }
 	  	}
 	});
 
